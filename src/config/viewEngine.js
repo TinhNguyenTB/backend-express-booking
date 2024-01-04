abb23838f21a1f38953b28a2352afa97a237bfa9
TinhNguyenTB <tinh271103@gmail.com>
2024-01-04 08:58:59 +0700

    init project

---
+++ src/config/viewEngine.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+let configViewEngine = (app) => {
+    app.use(express.static("./src/public"));  // client chi co the truy cap vao cac file trong thu muc public
+    app.set("view engine", "ejs");  // su dung view engine la ejs
+    app.set("views", ".src/views");  // duong dan den cac file view
+}
+
+module.exports = configViewEngine;
