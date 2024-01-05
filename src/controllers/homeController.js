4256f638038bc7a1fe8627a79c38e12854be3748
TinhNguyenTB <tinh271103@gmail.com>
2024-01-05 08:15:14 +0700

    crud create user using sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index"
+import CRUDservice from "../services/CRUDservice";
 
 let getHomePage = async (req, res) => {
     try {
@@ -13,8 +14,9 @@ let getCRUD = (req, res) => {
     return res.render('crud.ejs');
 }
 
-let postCRUD = (req, res) => {
-    console.log(req.body)
+let postCRUD = async (req, res) => {
+    let message = await CRUDservice.createNewUser(req.body)
+    console.log(message)
     return res.send('post crud from server');
 }
 
