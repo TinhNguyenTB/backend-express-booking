256eda25992dbecfefff48694ff3a9f9822ae64c
TinhNguyenTB <tinh271103@gmail.com>
2024-01-06 09:32:25 +0700

    crud delete user using sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -22,15 +22,11 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDservice.getAllUser()
-    // console.log('--------------------------')
-    // console.log(data)
-    // console.log('--------------------------')
     return res.render('displayCRUD.ejs', { dataUser: data })
 }
 
 let getEditCRUD = async (req, res) => {
     let userId = req.query.id;
-
     if (userId) {
         let userData = await CRUDservice.getUserInfoById(userId)
         return res.render('editCRUD.ejs', { user: userData })
@@ -38,7 +34,6 @@ let getEditCRUD = async (req, res) => {
     else {
         return res.send('Users not found')
     }
-
 }
 
 let putCRUD = async (req, res) => {
@@ -47,11 +42,23 @@ let putCRUD = async (req, res) => {
     return res.render('displayCRUD.ejs', { dataUser: allUsers })
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDservice.deleteUserById(id)
+        return res.send("delete user succeed")
+    }
+    else {
+        return res.send("User not found")
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
