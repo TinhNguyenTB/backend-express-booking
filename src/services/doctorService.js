134ca61a6b49ea8b477fdfc110cb9017a94895c8
TinhNguyenTB <tinh271103@gmail.com>
2024-01-25 21:13:30 +0700

    fix api get detail doctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -87,15 +87,21 @@ let getDetailDoctorById = (inputId) => {
                 let data = await db.User.findOne({
                     where: { id: inputId },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']  // not get password
                     },
                     include: [
                         { model: db.Markdown, attributes: ['description', 'contentHTML', 'contentMarkdown'] },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] }
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+                if (!data) {
+                    data = {}
+                }
                 resolve({
                     errCode: 0,
                     data: data
